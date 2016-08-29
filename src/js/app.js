88aa485950c33a3cbbcee69f89080c8b533cbd60
Murillo de Morais <murillo.morais@vianuvem.com.br>
2016-08-29 17:00:54 -0300

    fix: corrected saved events from localStorage

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-	console.log('-- jQuery OK --');
+
+	var saved_events = getSavedEvents();
 
 	$('input').each(function(){
 		this.onblur = function(){
@@ -24,40 +25,6 @@ $(document).ready(function(){
 	});
 
 	$('form[name="createEvent"]').unbind('submit').on('submit',function(){
-
-		var saved_events = JSON.parse(localStorage.getItem('events'));
-
-		if (saved_events === null || (Array.isArray(saved_events) && saved_events.length == 3))
-			saved_events = [
-								{
-									name: 'Jonas Bday!',
-									eventType: 'Birthday',
-									host: 'Jonas Apartment',
-									guestlist: 'Adrian, Maila, Carla..',
-									start: '2016-08-18T20:00',	
-									end: '2016-08-18T23:00',
-									additionalinfo: ''
-								},
-								{
-									name: 'Mobile Conference',
-									eventType: 'Conference',
-									host: 'Hilton Hotel',
-									guestlist: 'Mr. Carlos, Mr Pepper, Domenico..',
-									start: '2016-08-18T23:00',	
-									end: '2016-08-19T01:00',
-									additionalinfo: 'Bring your invite for access'
-								},
-								{
-									name: 'Juliana and Marc Marriage!',
-									eventType: 'Wedding',
-									host: 'Angels Church',
-									guestlist: 'Julien, Alberto, Peter..',
-									start: '2016-08-18T16:00',	
-									end: '2016-08-18T20:00',
-									additionalinfo: ''
-								}
-							];
-
 		var data = $(this).serialize().split("&");
 		var obj = {};
 		for(var key in data) {
@@ -72,18 +39,16 @@ $(document).ready(function(){
 		return false;
 	});
 
-	var saved_events = JSON.parse(localStorage.getItem('events'));
-
 	for(var i = 0; i < saved_events.length; i++){
 
 		var event =   '<div class="col-xs-12 col-md-3" style="margin-bottom:20px;">'
-					+ '	<div class="col-xs-12" style="background-color:#ddd;height:300px;">'
-					+ '		<h4 style="margin-top:20px;">' + saved_events[i].name + '</h3>'
+					+ '	<div class="col-xs-12" style="background-color:#ddd;height:230px;">'
+					+ '		<h4 style="margin-top:20px;">' + decodeURI(saved_events[i].name) + '</h3>'
 					+ '		<hr>'
-					+ '		<p style="text-align:left"><strong>Event Type:</strong> ' + saved_events[i].eventType + '</p>'
-					+ '		<p style="text-align:left"><strong>Host:</strong> ' + saved_events[i].host + '</p>'
-					+ '		<p style="text-align:left"><strong>Start:</strong> ' + saved_events[i].start + '</p>'
-					+ '		<p style="text-align:left"><strong>End:</strong> ' + saved_events[i].end + '</p>'
+					+ '		<p style="text-align:left"><strong>Event Type:</strong> ' + decodeURI(saved_events[i].eventType) + '</p>'
+					+ '		<p style="text-align:left"><strong>Host:</strong> ' + decodeURI(saved_events[i].host) + '</p>'
+					+ '		<p style="text-align:left"><strong>Start:</strong> ' + saved_events[i].start.substr(0,10) + '</p>'
+					+ '		<p style="text-align:left"><strong>End:</strong> ' + saved_events[i].end.substr(0,10) + '</p>'
 					+ '	</div>'
 					+ '</div>'
 		$('#eventsList').append(event);
@@ -91,6 +56,43 @@ $(document).ready(function(){
 	}
 
 });
+
+function getSavedEvents(){
+	var saved_events = JSON.parse(localStorage.getItem('events'));
+
+	if (saved_events === null || (Array.isArray(saved_events) && saved_events.length == 3))
+		saved_events = [
+							{
+								name: 'Jonas Bday!',
+								eventType: 'Birthday',
+								host: 'Jonas Apartment',
+								guestlist: 'Adrian, Maila, Carla..',
+								start: '2016-08-18T20:00',	
+								end: '2016-08-18T23:00',
+								additionalinfo: ''
+							},
+							{
+								name: 'Mobile Conference',
+								eventType: 'Conference',
+								host: 'Hilton Hotel',
+								guestlist: 'Mr. Carlos, Mr Pepper, Domenico..',
+								start: '2016-08-18T23:00',	
+								end: '2016-08-19T01:00',
+								additionalinfo: 'Bring your invite for access'
+							},
+							{
+								name: 'Juliana and Marc Marriage!',
+								eventType: 'Wedding',
+								host: 'Angels Church',
+								guestlist: 'Julien, Alberto, Peter..',
+								start: '2016-08-18T16:00',	
+								end: '2016-08-18T20:00',
+								additionalinfo: ''
+							}
+						];
+
+	return saved_events;
+}
 
 function refreshProgress(){
 	var step = 100 / $('.form-group').length;
